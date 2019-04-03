0af510b13ef39f0bfbe025b3864d181b979ec246
Andrew <digitaldox@live.com>
2019-04-03 08:02:26 -0700

    Added Point class

--- 03/03.py
+++ 03/03.py
@@ -23,23 +23,60 @@ def parse(file):
         items = line.split(' ')
         offset = tuple(map(int, items[-2][:-1].split(',')))
         size = tuple(map(int, items[-1].split('x')))
-        data.append((offset, size))
+        data.append((items[0][1:], offset, size))
     return data
 
 
 class Rectangle(object):
-    def __init__(self, offset, size):
-        self.top_left = (offset[0] + 1, offset[1] +1)
-        self.bottom_right = (self.top_left[0] + size[0], self.top_left[1] + size[1])
+    def __init__(self, parameters):
+        self.index, offset, size = parameters
+        self.top_left = Point(offset[0] + 1, offset[1] + 1)
+        self.bottom_right = Point(self.top_left[0] + size[0], self.top_left[1] + size[1])
 
     def intersection(self, other):
-        pass
+        if self.top_left >= other.top_left:
+            if self.top_left <= other.bottom_right:
+                return True
+        if self.bottom_right <= other.bottom_right:
+            if self.bottom_right >= other.top_left:
+                return True
+        return False
+
+    def __repr__(self):
+        return 'Rectangle {0} {1}x{2}'.format(self.index, self.top_left, self.bottom_right)
+
+
+class Point(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):
+        return True if self.x == other.y and self.y == other.y else False
+
+    def __gt__(self, other):
+        return True if self.x > other.x and self.y > other.y else False
+
+    def __ge__(self, other):
+        return True if self.x >= other.x and self.y >= other.y else False
+
+    def __lt__(self, other):
+        return True if self.x < other.x and self.y < other.y else False
+
+    def __le__(self, other):
+        return True if self.x <= other.x and self.y <= other.y else False
+
+    def __getitem__(self, item):
+        return self.y if item else self.x
+
+    def __repr__(self):
+        return '({0},{1})'.format(self.x, self.y)
 
 
 def solve_1(data):
     # Build quadrants
     quads = []
-    for offset, size in data:
+    for _, offset, size in data:
         for w in range(size[0]):
             x = offset[0] + w
             for h in range(size[1]):
@@ -59,6 +96,11 @@ def solve_1(data):
 
 
 def solve_2(data):
+    swatches = tuple(map(Rectangle, data))
+    for swatch in swatches:
+        intersections = [swatch.intersection(x) for x in swatches]
+        if not all(intersections):
+            return swatch
     return None
 
 
