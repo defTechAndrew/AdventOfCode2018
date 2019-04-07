f6d2620bcc0093e9c29a343f03478c955b2afde5
Andrew <digitaldox@live.com>
2019-04-07 12:00:25 -0700

    Finished Day 04

--- 04/04.py
+++ 04/04.py
@@ -1,5 +1,6 @@
-import unittest
 import datetime
+import re
+import unittest
 
 
 class Day04Test(unittest.TestCase):
@@ -7,13 +8,13 @@ class Day04Test(unittest.TestCase):
         data = parse(open("04.txt"))
         answer = solve_1(data)
         print('Answer 01', answer)
-        self.assertEqual(0, answer)
+        self.assertEqual(109659, answer)
 
-    # def test_2(self):
-    #     data = parse(open("04.txt"))
-    #     answer = solve_2(data)
-    #     print('Answer 02', answer)
-    #     self.assertEqual(0, answer)
+    def test_2(self):
+        data = parse(open("04.txt"))
+        answer = solve_2(data)
+        print('Answer 02', answer)
+        self.assertEqual(36371, answer)
 
 
 def parse(file):
@@ -32,6 +33,9 @@ def parse(file):
 
 
 def event_sets(l):
+    """
+    Generator to build guard shifts.
+    """
     guard_event = 0
     for i, time in enumerate(l):
         if time.event.startswith('Guard') and i != guard_event:
@@ -40,14 +44,25 @@ def event_sets(l):
 
 
 def merge_guard_ids(guard_shifts):
+    """
+    Merge event_sets into dictionary keyed by guard IDs.
+    """
     guard_times = dict()
     for event_set in guard_shifts:
-        guard_id = event_set[0].event.split()[1][1:]
+        guard_id = int(re.search(r'\d+', event_set[0].event).group(0))
         if guard_id in guard_times:
-            guard_times[guard_id].extend(event_set[1:])
+            guard_times[guard_id].extend(pair(event_set[1:]))
         else:
-            guard_times[guard_id] = event_set[1:]
+            guard_times[guard_id] = pair(event_set[1:])
     return guard_times
+
+
+def pair(times):
+    """
+    Turns sequential list into pairs of start and stop.
+    """
+    time_iter = iter(times)
+    return list(zip(time_iter, time_iter))
 
 
 class DateTime(datetime.datetime):
@@ -57,16 +72,58 @@ class DateTime(datetime.datetime):
         return dt
 
 
+def total_minutes_slept(time_ranges):
+    """
+    Find total minutes slept in all time ranges.
+    """
+    result = 0
+    for start, end in time_ranges:
+        result += end.minute - start.minute
+    return result
+
+
+def most_overlap(time_ranges):
+    """
+    Find instances of range overlaps and the minute it happens on.
+    """
+    minutes = 60
+    overlap = [0] * minutes
+    for minute in range(minutes):
+        for start, end in time_ranges:
+            if all([minute >= start.minute, minute < end.minute]):
+                overlap[minute] += 1
+
+    max_value = max(overlap)
+    return max_value, overlap.index(max_value)
+
+
 def solve_1(data):
     guard_dict = merge_guard_ids(data)
-    for key, value in guard_dict.items():
-        print(key, value)
+    max_slept = 0
+    sleepiest_guard = None
 
-    return None
+    for guard, time_ranges in guard_dict.items():
+        total = total_minutes_slept(time_ranges)
+        if total > max_slept:
+            max_slept = total
+            sleepiest_guard = guard
+
+    _, overlap_minute = most_overlap(guard_dict[sleepiest_guard])
+    return sleepiest_guard * overlap_minute
 
 
 def solve_2(data):
-    return None
+    guard_dict = merge_guard_ids(data)
+    most_overlap_times = 0
+    sleepiest_guard = None
+
+    for guard, time_ranges in guard_dict.items():
+        overlap, overlap_min = most_overlap(time_ranges)
+        if overlap > most_overlap_times:
+            most_overlap_times = overlap
+            sleepiest_guard = (guard, overlap_min)
+
+    return sleepiest_guard[0] * sleepiest_guard[1]
 
 
 if __name__ == '__main__':
